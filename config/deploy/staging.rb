73de4b9fa370cf0665273b2ed6052293a88f2662
Joydeep Banerjee <joydeep.banerjee@quantuminventions.com>
2020-09-07 00:24:40 +0530

    Hot fix

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server '13.127.64.13', user: 'deploy', roles: %w{web app db}
+server '15.207.19.163', user: 'deploy', roles: %w{web app db}
 set :branch, "master"
 set :rails_env, "staging"
 set :deploy_to, "/var/www/test_app" 
